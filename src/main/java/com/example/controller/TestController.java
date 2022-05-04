3711aa184b40ff313ec8f21872bdf25047cd54c6
rkatanor <rkatanor@gmail.com>
2022-05-04 08:00:39 +0530

    changed controller class

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	    
-	    @RequestMapping(value = "/hello")
+	    @RequestMapping(value = "/hello springboot")
 		public String helloMethod() {
 			return "hello ..";
 		}
